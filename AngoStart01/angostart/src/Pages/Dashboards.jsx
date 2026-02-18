46a4afb5c0bc9d886b0c215d331b19cdf18e2515
janobernardo2007-ctrl <janobernardo2007@gmail.com>
2026-02-18 19:17:53 +0100

    dark mode 1

--- AngoStart01/angostart/src/Pages/Dashboards.jsx
+++ AngoStart01/angostart/src/Pages/Dashboards.jsx
@@ -1,6 +1,6 @@
 import { useState , useEffect } from "react";
 import { Link } from "react-router-dom";
-import '../App.css';
+import '../style/auth.css';
 
 const navigationConfig = {
  empreendedor: [
